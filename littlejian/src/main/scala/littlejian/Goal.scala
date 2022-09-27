3124963f03587d51b40f9313d7d9bd7722029ba3
ㄗㄠˋ ㄑㄧˊ <tsao-chi@the-lingo.org>
2022-09-27 23:59:20 +0800

    up

--- littlejian/src/main/scala/littlejian/Goal.scala
+++ littlejian/src/main/scala/littlejian/Goal.scala
@@ -5,20 +5,28 @@ import scala.collection.parallel.immutable.ParVector
 sealed trait Goal
 
 sealed trait GoalBasic extends Goal {
-  def execute(state: State): Option[State] = ??? // TODO
+  def execute(state: State): Option[State]
 }
 
-final case class GoalEq[T](x: VarOr[T], y: VarOr[T])(implicit unifier: Unifier[T]) extends GoalBasic
+final case class GoalEq[T](x: VarOr[T], y: VarOr[T])(implicit unifier: Unifier[T]) extends GoalBasic {
+  override def execute(state: State): Option[State] = ???
+}
 
-final case class GoalNotEq[T](x: VarOr[T], y: VarOr[T])(implicit unifier: Unifier[T]) extends GoalBasic
+final case class GoalNotEq[T](x: VarOr[T], y: VarOr[T])(implicit unifier: Unifier[T]) extends GoalBasic {
+  override def execute(state: State): Option[State] = ???
+}
 
 import scala.reflect.ClassTag
 
 type PredTypeTag = ClassTag[_]
 
-final case class GoalPredType[T](tag: PredTypeTag, x: VarOr[T])(implicit unifier: Unifier[T]) extends GoalBasic
+final case class GoalPredType[T](tag: PredTypeTag, x: VarOr[T])(implicit unifier: Unifier[T]) extends GoalBasic {
+  override def execute(state: State): Option[State] = ???
+}
 
-final case class GoalPredNotType[T](tag: PredTypeTag, x: VarOr[T])(implicit unifier: Unifier[T]) extends GoalBasic
+final case class GoalPredNotType[T](tag: PredTypeTag, x: VarOr[T])(implicit unifier: Unifier[T]) extends GoalBasic {
+  override def execute(state: State): Option[State] = ???
+}
 
 sealed trait GoalControl extends Goal
 
@@ -32,6 +40,7 @@ final case class GoalConj(xs: ParVector[Goal]) extends GoalControl
 
 object Goal {
   def conde(clauses: ParVector[ParVector[Goal]]): Goal = GoalDisj(clauses.map(GoalConj(_)))
+
   val success: Goal = GoalConj(ParVector())
   val failure: Goal = GoalDisj(ParVector())
 }

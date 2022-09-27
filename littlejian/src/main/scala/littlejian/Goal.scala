a1c44df205c36ffa6689c658922760034acb5a2c
ㄗㄠˋ ㄑㄧˊ <tsao-chi@the-lingo.org>
2022-09-27 15:22:16 +0800

    goal controls

--- littlejian/src/main/scala/littlejian/Goal.scala
+++ littlejian/src/main/scala/littlejian/Goal.scala
@@ -1,5 +1,7 @@
 package littlejian
 
+import scala.collection.parallel.immutable.ParVector
+
 sealed trait Goal
 
 sealed trait GoalBasic extends Goal
@@ -13,3 +15,17 @@ import scala.reflect.ClassTag
 final case class GoalPredType[T](cls: ClassTag[_], x: VarOr[T])(implicit unifier: Unifier[T]) extends GoalBasic
 
 final case class GoalPredNotType[T](cls: ClassTag[_], x: VarOr[T])(implicit unifier: Unifier[T]) extends GoalBasic
+
+sealed trait GoalControl extends Goal
+
+final class GoalDelay(x: => Goal) extends GoalControl {
+  lazy val get: Goal = x
+}
+
+final case class GoalDisj(xs: ParVector[Goal]) extends GoalControl
+
+final case class GoalConj(xs: ParVector[Goal]) extends GoalControl
+
+object Goal {
+  def conde(clauses: ParVector[ParVector[Goal]]): Goal = GoalDisj(clauses.map(GoalConj(_)))
+}

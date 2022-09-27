98504f5f03cd4791581658da4e6aa5398689f8a0
ㄗㄠˋ ㄑㄧˊ <tsao-chi@the-lingo.org>
2022-09-28 00:51:58 +0800

    hole = new Var

--- littlejian/src/main/scala/littlejian/Var.scala
+++ littlejian/src/main/scala/littlejian/Var.scala
@@ -1,7 +1,9 @@
 package littlejian
 
 // using reference equality
-final class Var[T](unifier: Unifier[T]) {
+final class Var[T](implicit unifier: Unifier[T]) {
 }
+
+def hole[T](implicit unifier: Unifier[T]) = new Var[T]()(unifier)
 
 type VarOr[T] = Var[T] | T

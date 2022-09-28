b24a2dcfb559d4853b90c18fbedd4d7a9272ddd7
ㄗㄠˋ ㄑㄧˊ <tsao-chi@the-lingo.org>
2022-09-28 09:08:40 +0800

    relUnitToGoal

--- littlejian/src/main/scala/littlejian/ext/GoalWith.scala
+++ littlejian/src/main/scala/littlejian/ext/GoalWith.scala
@@ -1,4 +1,4 @@
-package littlejian.withval
+package littlejian.ext
 
 import littlejian._
 
@@ -14,3 +14,7 @@ implicit def lambdaToRel[T](fn: VarOr[T] => Goal)(implicit unifier: Unifier[T]):
   val result = hole
   GoalWith(fn(result), result)
 }
+
+implicit def relUnitToGoal(rel: Rel[Unit]): Goal = if(rel.x != ()) throw new IllegalStateException("x != ()") else rel.goal
+
+implicit def goalToRelUnit(goal: Goal): Rel[Unit] = GoalWith(goal, ())

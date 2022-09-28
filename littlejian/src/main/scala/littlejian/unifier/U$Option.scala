ff39383951e66d2bc407f973a55b057aca4608bb
ㄗㄠˋ ㄑㄧˊ <tsao-chi@the-lingo.org>
2022-09-28 12:50:27 +0800

    U$Option

--- littlejian/src/main/scala/littlejian/unifier/U$Option.scala
+++ littlejian/src/main/scala/littlejian/unifier/U$Option.scala
@@ -5,10 +5,8 @@ import littlejian._
 
 import scala.language.implicitConversions
 
-implicit def U$Option[T](implicit unifier: Unifier[T]): Unifier[Option[T]] = new Unifier[Option[T]] {
-  override def concreteUnify(self: Option[T], other: Option[T]): Unifying[Unit] = (self, other) match {
-    case (Some(x), Some(y)) => x.unify(y)
-    case (None, None) => Unifying.success(())
-    case _ => Unifying.failure
-  }
+implicit def U$Option[T](implicit unifier: Unifier[T]): Unifier[Option[T]] = (self: Option[T], other: Option[T]) => (self, other) match {
+  case (Some(x), Some(y)) => x.unify(y)
+  case (None, None) => Unifying.success(())
+  case _ => Unifying.failure
 }

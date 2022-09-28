6848fdde75548a89380ab7386d5a6579852a1187
ㄗㄠˋ ㄑㄧˊ <tsao-chi@the-lingo.org>
2022-09-28 10:17:54 +0800

    first test

--- littlejian/src/test/scala/MySuite.scala
+++ littlejian/src/test/scala/MySuite.scala
@@ -1,9 +1,15 @@
-// For more information on writing tests, see
-// https://scalameta.org/munit/docs/getting-started.html
+import littlejian._
+import littlejian.ext._
+import littlejian.search.naive._
+
+
 class MySuite extends munit.FunSuite {
   test("example test that succeeds") {
     val obtained = 42
     val expected = 42
     assertEquals(obtained, expected)
+  }
+  test("basics") {
+    assertEquals(run { (x: VarOr[Int]) => x === 42 }, "42\n")
   }
 }

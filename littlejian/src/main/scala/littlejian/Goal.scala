c1202126d48bbcdab1797cbd3ebb88dedda34e65
ㄗㄠˋ ㄑㄧˊ <tsao-chi@the-lingo.org>
2022-09-29 09:22:34 +0800

    up

--- littlejian/src/main/scala/littlejian/Goal.scala
+++ littlejian/src/main/scala/littlejian/Goal.scala
@@ -61,20 +61,19 @@ final class GoalDelay(x: => Goal) extends GoalControl {
 
   override def toString: String = {
     val level = goalDelayEvalLevel.get.getOrElse(defaultGoalDelayEvalLevel)
-    if (level <= 0) super.toString else this.get.toString
+    if (level <= 0) super.toString else goalDelayEvalLevel.callWith(level - 1) {
+      this.get.toString
+    }
   }
 }
 
 final case class GoalDisj(xs: ParVector[Goal]) extends GoalControl {
-  /*
   private def flatten: ParVector[Goal] = xs.map({
     case x: GoalDisj => x.flatten
     case v => ParVector(v)
   }).flatten
 
   override def toString: String = s"conde(${this.flatten.mkString(", ")})"
-  */
-  override def toString: String = s"conde(${xs.mkString(", ")})"
 }
 
 object GoalDisj {
@@ -86,15 +85,12 @@ object GoalDisj {
 }
 
 final case class GoalConj(xs: ParVector[Goal]) extends GoalControl {
-  /*
   private def flatten: ParVector[Goal] = xs.map({
     case x: GoalConj => x.flatten
     case v => ParVector(v)
   }).flatten
 
   override def toString: String = s"begin(${this.flatten.mkString(", ")})"
-  */
-  override def toString: String = s"begin(${xs.mkString(", ")})"
 }
 
 object GoalConj {

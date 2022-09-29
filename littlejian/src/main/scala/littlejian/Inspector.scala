c66ac06a67ca6f5aa6e34d4129537b77984855d3
ㄗㄠˋ ㄑㄧˊ <tsao-chi@the-lingo.org>
2022-09-29 23:49:44 +0800

    up

--- littlejian/src/main/scala/littlejian/Inspector.scala
+++ littlejian/src/main/scala/littlejian/Inspector.scala
@@ -13,7 +13,6 @@ final case class WithInspector[T](x: VarOr[T])(implicit inspector: Inspector[T])
   // None: contains
   // Some(Seq()): not contains
   // Some(Seq(...)): uncertain
-  // todo: catch recursion scan
   final def scanUncertain(resolver: Any => Any, v: Any): Option[Seq[WithInspector[_]]] = {
     val todo = resolver(x).asInstanceOf[VarOr[T]]
     val history = scanRecHistory.get.getOrElse(HashSet.empty)

b718f79f51dee4fb45ca1833ea75bcfbecf76439
GreatHaop <102635645+GreatHaop@users.noreply.github.com>
2022-09-30 10:02:05 +0800

    collect

--- littlejian/src/main/scala/littlejian/ext/condp.scala
+++ littlejian/src/main/scala/littlejian/ext/condp.scala
@@ -2,9 +2,23 @@ package littlejian.ext
 
 import littlejian._
 
-def condp(keys: Seq[String]*)(clauses: => (String, Goal)*): Goal = GoalDelay(???)
+final case class Walker(f: Any => Any) {
+  def apply[T](x: VarOr[T]): VarOr[T] = f(x).asInstanceOf[VarOr[T]]
+}
 
-def condp[T](keys: Seq[String]*)(clauses: => (String, Rel[T])*)(implicit unifier: Unifier[T]): Rel[T] = {
+val UseMaybe = "use-maybe"
+
+private def collect(subst: Subst, keys: Seq[Walker => Seq[String]]*): Seq[String] =
+  if (keys.isEmpty) Seq.empty
+  else {
+    val walker = Walker(subst.walk)
+    val ulos = keys.head.flatMap(_ (walker))
+    if (ulos.contains(UseMaybe)) ulos ++ collect(subst, keys.tail *) else ulos
+  }
+
+def condp(keys: Seq[Walker => Seq[String]]*)(clauses: => (String, Goal)*): Goal = GoalDelay(???)
+
+def condp[T](keys: Seq[Walker => Seq[String]]*)(clauses: => (String, Rel[T])*)(implicit unifier: Unifier[T]): Rel[T] = {
   val v = new Var[T]
   GoalWith(GoalDelay {
     condp(keys *)(clauses.map { case (id, rel) => (id, GoalConj(rel.goal, rel.x === v)) } *)

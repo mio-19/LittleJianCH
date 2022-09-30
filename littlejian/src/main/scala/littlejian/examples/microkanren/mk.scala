6eb4a98d741532fb74b740be2cabdfeaf11955d1
ㄗㄠˋ ㄑㄧˊ <tsao-chi@the-lingo.org>
2022-09-30 22:51:24 +0800

    up

--- littlejian/src/main/scala/littlejian/examples/microkanren/mk.scala
+++ littlejian/src/main/scala/littlejian/examples/microkanren/mk.scala
@@ -17,11 +17,15 @@ implicit val U$MKPair: Unifier[MKPair] = U$Product
 
 type MKMap = MKMapEmpty.type | MKMapCons
 implicit val U$MKMap: Unifier[MKMap] = U$Union[MKMapEmpty.type, MKMapCons]
+implicit val U$VarOr$MKMap: Unifier[VarOr[MKMap]] = U$VarOr(U$MKMap)
+
 case object MKMapEmpty
+
 implicit val U$MKMapEmpty: Unifier[MKMapEmpty.type] = equalUnifier
 
 final case class MKMapCons(key: VarOr[MKData], value: VarOr[MKData], tail: VarOr[MKMap]) extends Product3[VarOr[MKData], VarOr[MKData], VarOr[MKMap]]
-implicit val U$MKMapCons: Unifier[MKMapCons] = U$Product(U$VarOr(U$MKData), U$VarOr(U$MKData), U$VarOr(U$MKMap))
+
+implicit val U$MKMapCons: Unifier[MKMapCons] = U$Product
 
 sealed trait MKThunkKind
 
@@ -35,29 +39,29 @@ object MKThunkKind {
 
 implicit val U$MKThunkKind: Unifier[MKThunkKind] = equalUnifier
 
-final case class MKThunk(kind: VarOr[MKThunkKind], env: VarOr[MKData], rand: VarOr[MKData], s: VarOr[MKData], c: VarOr[MKData]) extends Product5[VarOr[MKThunkKind], VarOr[MKData], VarOr[MKData], VarOr[MKData], VarOr[MKData]]
+final case class MKThunk(kind: VarOr[MKThunkKind], env: VarOr[MKMap], rand: VarOr[MKData], s: VarOr[MKData], c: VarOr[MKData]) extends Product5[VarOr[MKThunkKind], VarOr[MKMap], VarOr[MKData], VarOr[MKData], VarOr[MKData]]
 
 implicit val U$MKThunk: Unifier[MKThunk] = U$Product
 
 type MKGoal = (MKGoalEq | MKGoalCallFresh) | (MKGoalConj | MKGoalDisj) | MKGoalTop
 implicit val U$MKGoal: Unifier[MKGoal] = U$Union(U$Union[MKGoalEq, MKGoalCallFresh], U$Union[MKGoalConj, MKGoalDisj], U$MKGoalTop)
 
-final case class MKGoalEq(u: VarOr[MKData], v: VarOr[MKData], env: VarOr[MKData]) extends Product3[VarOr[MKData], VarOr[MKData], VarOr[MKData]]
+final case class MKGoalEq(u: VarOr[MKData], v: VarOr[MKData], env: VarOr[MKMap]) extends Product3[VarOr[MKData], VarOr[MKData], VarOr[MKMap]]
 
 implicit val U$MKGoalEq: Unifier[MKGoalEq] = U$Product
 
-final case class MKGoalCallFresh(f: VarOr[MKData], env: VarOr[MKData]) extends Product2[VarOr[MKData], VarOr[MKData]]
+final case class MKGoalCallFresh(f: VarOr[MKData], env: VarOr[MKMap]) extends Product2[VarOr[MKData], VarOr[MKMap]]
 
 implicit val U$MKGoalCallFresh: Unifier[MKGoalCallFresh] = U$Product
 
-final case class MKGoalConj(g1: VarOr[MKData], g2: VarOr[MKData], env: VarOr[MKData]) extends Product3[VarOr[MKData], VarOr[MKData], VarOr[MKData]]
+final case class MKGoalConj(g1: VarOr[MKData], g2: VarOr[MKData], env: VarOr[MKMap]) extends Product3[VarOr[MKData], VarOr[MKData], VarOr[MKMap]]
 
 implicit val U$MKGoalConj: Unifier[MKGoalConj] = U$Product
 
-final case class MKGoalDisj(g1: VarOr[MKData], g2: VarOr[MKData], env: VarOr[MKData]) extends Product3[VarOr[MKData], VarOr[MKData], VarOr[MKData]]
+final case class MKGoalDisj(g1: VarOr[MKData], g2: VarOr[MKData], env: VarOr[MKMap]) extends Product3[VarOr[MKData], VarOr[MKData], VarOr[MKMap]]
 
 implicit val U$MKGoalDisj: Unifier[MKGoalDisj] = U$Product
 
-final case class MKGoalTop(rand: VarOr[MKData], env: VarOr[MKData]) extends Product2[VarOr[MKData], VarOr[MKData]]
+final case class MKGoalTop(rand: VarOr[MKData], env: VarOr[MKMap]) extends Product2[VarOr[MKData], VarOr[MKMap]]
 
 implicit val U$MKGoalTop: Unifier[MKGoalTop] = U$Product

70a871351db6c1bd12f037af227ccf7461b677d3
ㄗㄠˋ ㄑㄧˊ <tsao-chi@the-lingo.org>
2022-10-02 21:30:31 +0800

    up

--- littlejian/src/main/scala/littlejian/search/ReducingSearcher.scala
+++ littlejian/src/main/scala/littlejian/search/ReducingSearcher.scala
@@ -9,7 +9,25 @@ implicit object ReducingSearcher extends Searcher {
   // TODO: parallel execution
   private def runBasics(state: State, xs: Seq[GoalBasic]): Option[State] = if (xs.isEmpty) Some(state) else xs.head.execute(state).flatMap(runBasics(_, xs.tail))
 
-  override def run(state: State, goal: Goal): Stream[State] = StateWithGoals(state, Vector(goal)).exec.toStream
+  def runSingle(state: State, goal: Goal): Stream[State] = StateWithGoals(state, Vector(goal)).exec.toStream
+
+  def runPar(state: State, goal: Goal): Stream[State] = exec(ParVector(StateWithGoals(state, Vector(goal)))).toStream
+
+  override def run(state: State, goal: Goal): Stream[State] = runPar(state, goal)
+
+  private val maxTasks = 8
+
+  def exec(xs: ParVector[StateWithGoals]): SStream[State] =
+    if (xs.isEmpty) SStream.empty
+    else {
+      if (xs.length > maxTasks) {
+        val (left, right) = xs.splitAt(xs.length / 2)
+        mplus(exec(left), exec(right))
+      }
+      val result = xs.map(_.run)
+      val (ok, rest) = (result.flatMap(_._1), result.flatMap(_._2))
+      SStream.append(ok, SDelay(exec(rest)))
+    }
 
   final case class StateWithGoals(state: State, goals: Vector[Goal]) {
     def reduceNotSplit: Option[(StateWithGoals, Vector[GoalDisj])] =
@@ -40,20 +58,26 @@ implicit object ReducingSearcher extends Searcher {
         }
       }
 
-    def split(xs: Seq[GoalDisj]): ParVector[StateWithGoals] =
+    def fullyReduceNotSplit: Option[(State, Vector[GoalDisj])] =
+      if (goals.isEmpty)
+        Some((state, Vector.empty))
+      else for {
+        (x, disjs) <- reduceNotSplit
+        (state, disjs2) <- x.fullyReduceNotSplit
+      } yield (state, disjs ++ disjs2)
+
+    def split1(xs: Seq[GoalDisj]): ParVector[StateWithGoals] =
       if (xs.isEmpty) ParVector(this)
       else {
-        val (head, tail) = (xs.head, xs.tail)
-        head.xs.map({ disj =>
-          StateWithGoals(state, disj +: goals)
-        }).flatMap(_.split(tail))
+        val gs = goals ++ xs.tail
+        xs.head.xs.map(g => StateWithGoals(state, g +: gs))
       }
 
     def run: (Option[State], ParVector[StateWithGoals]) =
       if (goals.isEmpty) (Some(state), ParVector.empty)
-      else this.reduceNotSplit match {
+      else this.fullyReduceNotSplit match {
         case None => (None, ParVector.empty)
-        case Some((state, disjs)) => (None, state.split(disjs))
+        case Some((state, disjs)) => (None, StateWithGoals(state, Vector.empty).split1(disjs))
       }
 
     def exec: SStream[State] = this.run match {

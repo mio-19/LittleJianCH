fe61e3d194c31b3b05bac3b0d1059448885395f2
ㄗㄠˋ ㄑㄧˊ <tsao-chi@the-lingo.org>
2022-10-03 11:04:02 +0800

    up

--- littlejian/src/main/scala/littlejian/search/BFSimp.scala
+++ littlejian/src/main/scala/littlejian/search/BFSimp.scala
@@ -4,6 +4,13 @@ import littlejian._
 
 implicit object BFSimp extends Searcher {
   final class SizedStream[T](val bucket: Vector[T], val thunk: Option[() => SizedStream[T]]) {
+    def appendFair(other: SizedStream[T]): SizedStream[T] = {
+      new SizedStream(bucket ++ other.bucket, (thunk, other.thunk) match {
+        case (None, xs) => xs
+        case (xs, None) => xs
+        case (Some(xs), Some(ys)) => Some(() => xs().appendFair(ys()))
+      })
+    }
   }
 
   object SizedStream {
@@ -11,6 +18,6 @@ implicit object BFSimp extends Searcher {
 
     def apply[T](thunk: => SizedStream[T]): SizedStream[T] = new SizedStream[T](Vector.empty, Some(() => thunk))
   }
-  
+
   override def run(state: State, goal: Goal): Stream[State] = ???
 }

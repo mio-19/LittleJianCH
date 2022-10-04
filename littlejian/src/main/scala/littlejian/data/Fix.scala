7a6642e66d6d6274ff3a38548a672c2d1cc97f5e
ㄗㄠˋ ㄑㄧˊ <tsao-chi@the-lingo.org>
2022-10-04 18:00:42 +0800

    fix the hack

--- littlejian/src/main/scala/littlejian/data/Fix.scala
+++ littlejian/src/main/scala/littlejian/data/Fix.scala
@@ -4,42 +4,18 @@ import littlejian._
 
 import scala.language.implicitConversions
 
-final implicit class Fix[F[_]](val out: F[Fix[F]]) extends Product1[F[Fix[F]]] {
-  override def _1: F[Fix[F]] = out
 
-  override def toString: String = out.toString
+type Fix[F[_]] = F[F[F[F[Any]]]]
 
-  override def hashCode: Int = out.hashCode()
+//def fix[F[_]](out: F[Fix[F]]): Fix[F[_]] = out.asInstanceOf[Fix[F[_]]]
 
-  override def equals(obj: Any): Boolean = obj match {
-    case that: Fix[_] => out == that.out
-    case _ => out == obj
-  }
+implicit def U$Fix[F[_]](implicit U$F: Unifier[F[Fix[F]]]): Unifier[Fix[F]] = U$F.asInstanceOf[Unifier[Fix[F]]]
+implicit def I$Fix[F[_]](implicit I$F: Inspector[F[Fix[F]]]): Inspector[Fix[F]] = I$F.asInstanceOf[Inspector[Fix[F]]]
 
-  override def canEqual(that: Any): Boolean = that.equals(out)
-}
+implicit def fix2unfix[F[_]](fix: Fix[F]): F[Fix[F]] = fix.asInstanceOf[F[Fix[F]]]
 
-implicit def U$Fix[F[_]](implicit U$F: Unifier[F[Fix[F]]]): Unifier[Fix[F]] = (((x: Any, y: Any) => (x, y) match {
-  case (x: Fix[_], y: Fix[_]) => U$F.unify(x.asInstanceOf[Fix[F]].out, y.asInstanceOf[Fix[F]].out)
-  case (x: Fix[_], y) => U$F.unify(x.asInstanceOf[Fix[F]].out, y.asInstanceOf[F[Fix[F]]])
-  case (x, y: Fix[_]) => U$F.unify(x.asInstanceOf[F[Fix[F]]], y.asInstanceOf[Fix[F]].out)
-  case (x, y) => U$F.unify(x.asInstanceOf[F[Fix[F]]], y.asInstanceOf[F[Fix[F]]])
-}): Unifier[Any]).asInstanceOf[Unifier[Fix[F]]]
-implicit def I$Fix[F[_]](implicit I$F: Inspector[F[Fix[F]]]): Inspector[Fix[F]] = (((x: Any) => x match {
-  case x: Fix[_] => I$F.inspect(x.asInstanceOf[Fix[F]].out)
-  case x => I$F.inspect(x.asInstanceOf[F[Fix[F]]])
-}): Inspector[Any]).asInstanceOf[Inspector[Fix[F]]]
+implicit def unfix2fix[F[_]](unfix: F[Fix[F]]): Fix[F] = unfix.asInstanceOf[Fix[F]]
 
-implicit def fix2unfix[F[_]](fix: Fix[F]): F[Fix[F]] = fix.out
+implicit def unfix2fixVarOr[F[_]](unfix: VarOr[F[Fix[F]]]): VarOr[Fix[F]] = unfix.asInstanceOf[VarOr[Fix[F]]]
 
-implicit def unfix2fix[F[_]](unfix: F[Fix[F]]): Fix[F] = new Fix(unfix)
-
-implicit def unfix2fixVarOr[F[_]](unfix: VarOr[F[Fix[F]]]): VarOr[Fix[F]] = unfix match {
-  case v: Var[_] => v.asInstanceOf[VarOr[Fix[F]]]
-  case out: F[Fix[F]] => new Fix(out)
-}
-
-implicit def fix2unfixVarOr[F[_]](fix: VarOr[Fix[F]]): VarOr[F[Fix[F]]] = fix match {
-  case v: Var[_] => v.asInstanceOf[VarOr[F[Fix[F]]]]
-  case out: Fix[F] => out.out
-}
+implicit def fix2unfixVarOr[F[_]](fix: VarOr[Fix[F]]): VarOr[F[Fix[F]]] = fix.asInstanceOf[VarOr[F[Fix[F]]]]
--- littlejian/src/main/scala/littlejian/data/sexp/Cons.scala
+++ littlejian/src/main/scala/littlejian/data/sexp/Cons.scala
@@ -1,32 +1,37 @@
 package littlejian.data.sexp
 
-import littlejian._
-import littlejian.ext._
-import littlejian.data._
+import littlejian.*
+import littlejian.ext.*
+import littlejian.data.*
 
 import scala.annotation.tailrec
+import scala.language.implicitConversions
 
 type SExp0[SExp] = Pair[SExp, SExp] | Unit | String
 type SExp = SExp0[Fix[SExp0]]
+implicit def zeroToSExp(x: Fix[SExp0]): SExp = x.asInstanceOf[SExp]
+implicit def sExpToZero(x: SExp): Fix[SExp0] = x.asInstanceOf[Fix[SExp0]]
+implicit def varorzeroToSExp(x: VarOr[Fix[SExp0]]): VarOr[SExp] = x.asInstanceOf[VarOr[SExp]]
+implicit def varorsExpTOzero(x: VarOr[SExp]): VarOr[Fix[SExp0]] = x.asInstanceOf[VarOr[Fix[SExp0]]]
 
 object SExp {
   def parse(s: String): SExp = {
     val (rest, r) = doParse(s)
     val s2 = rest.strip()
-    if(s2.isEmpty) r else throw new Exception(s"parse error: remaining $s2")
+    if (s2.isEmpty) r else throw new Exception(s"parse error: remaining $s2")
   }
 
   private def doParse(s: String): (String, SExp) = {
-    val (s1, s2) = s.strip().span(x=> x.isLetterOrDigit || x == '_' || x == '-')
-    if(s1.nonEmpty) return (s2, s1)
-    if(s2.isEmpty) throw new Exception("Invalid SExp")
+    val (s1, s2) = s.strip().span(x => x.isLetterOrDigit || x == '_' || x == '-')
+    if (s1.nonEmpty) return (s2, s1)
+    if (s2.isEmpty) throw new Exception("Invalid SExp")
     val c = s2.head
     val s3 = s2.tail
     c match {
       case '(' => {
         val xs = Seq.newBuilder[SExp]
         var rest = s3
-        while(!(rest.isEmpty || rest.head == ')' || rest.head == '.')){
+        while (!(rest.isEmpty || rest.head == ')' || rest.head == '.')) {
           val (s4, x) = doParse(rest)
           xs += x
           rest = s4.strip()
@@ -36,7 +41,7 @@ object SExp {
           case '.' => {
             val (s4, x) = doParse(rest.tail)
             val s5 = s4.strip()
-            if(s5.head != ')') throw new Exception("Invalid SExp")
+            if (s5.head != ')') throw new Exception("Invalid SExp")
             (s5.tail, convertListDot(xs.result(), x))
           }
         }
@@ -49,9 +54,9 @@ object SExp {
   }
 }
 
-implicit val U$SExp: Unifier[SExp] = U$Union(U$Pair, U$Unit, U$String)
+implicit val U$SExp: Unifier[SExp] = U$Union(U$Pair(U$Fix(U$SExp), U$Fix(U$SExp)), U$Unit, U$String)
 
-implicit val I$SExp: Inspector[SExp] = I$Union(I$Pair, I$Unit, I$String)
+implicit val I$SExp: Inspector[SExp] = I$Union(I$Pair(I$Fix(I$SExp), I$Fix(I$SExp)), I$Unit, I$String)
 
 private def consDot(xs: Vector[VarOr[SExp]], next: Var[SExp] | String): String =
   if (xs.isEmpty)
@@ -61,6 +66,7 @@ private def consDot(xs: Vector[VarOr[SExp]], next: Var[SExp] | String): String =
 
 object Cons {
   def apply(a: VarOr[SExp], d: VarOr[SExp]): SExp = Pair(a, d)
+
   def unapply(s: VarOr[SExp]): Option[(VarOr[SExp], VarOr[SExp])] = s match {
     case Pair(a, d) => Some((a, d))
     case _ => None
@@ -78,17 +84,20 @@ private def convertList(xs: Seq[VarOr[SExp]]): SExp = if (xs.isEmpty) () else co
 private def convertListDot(xs: Seq[VarOr[SExp]]): VarOr[SExp] = {
   val head = xs.head
   val tail = xs.tail
-  if(tail.isEmpty) head else cons(head, convertListDot(tail))
+  if (tail.isEmpty) head else cons(head, convertListDot(tail))
 }
 private def convertListDot(xs: Seq[VarOr[SExp]], x: SExp): SExp = convertListDot(xs :+ x).asInstanceOf[SExp]
+
 object list {
   def apply(xs: VarOr[SExp]*): SExp = convertList(xs)
+
   def unapplySeq(x: VarOr[SExp]): Option[Seq[VarOr[SExp]]] = x match {
     case Cons(a, d) => unapplySeq(d).map(a +: _)
     case () => Some(Seq())
     case _ => None
   }
 }
+
 def listDot(xs: VarOr[SExp]*) = convertListDot(xs)
 
 def mapo(f: VarOr[SExp] => Rel[SExp], xs: VarOr[SExp]): Rel[SExp] = conde(

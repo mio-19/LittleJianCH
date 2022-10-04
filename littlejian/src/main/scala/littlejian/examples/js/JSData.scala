476458a7420a49a162ea678e3b6dbaf7685d5182
ㄗㄠˋ ㄑㄧˊ <tsao-chi@the-lingo.org>
2022-10-04 18:47:05 +0800

    up

--- littlejian/src/main/scala/littlejian/examples/js/JSData.scala
+++ littlejian/src/main/scala/littlejian/examples/js/JSData.scala
@@ -1,5 +1,13 @@
 package littlejian.examples.js
 
-import littlejian.data.Nat
+import littlejian._
+import littlejian.data.*
 
-type JSData = Unit | Nat 
+import scala.language.implicitConversions
+
+type JSData0[JSData] = LList[JSData] | String | Boolean
+type JSData = JSData0[JSData0[Any]]
+implicit def jsData0ToJSData[T](x: JSData0[T]): JSData = x.asInstanceOf[JSData]
+implicit def jsDataToJSData0[T](x: JSData): JSData0[T] = x.asInstanceOf[JSData0[T]]
+
+implicit val U$JSData: Unifier[JSData] = U$Union(U$LList(U$JSData), U$String, U$Boolean).asInstanceOf[Unifier[JSData]]

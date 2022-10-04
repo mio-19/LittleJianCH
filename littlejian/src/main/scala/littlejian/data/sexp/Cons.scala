64e7854bf979e8886fa3be78f79df40bbaf79f87
ㄗㄠˋ ㄑㄧˊ <tsao-chi@the-lingo.org>
2022-10-04 16:06:20 +0800

    SExp.parse()

--- littlejian/src/main/scala/littlejian/data/sexp/Cons.scala
+++ littlejian/src/main/scala/littlejian/data/sexp/Cons.scala
@@ -7,6 +7,46 @@ import scala.annotation.tailrec
 
 type SExp = Cons | Unit | String
 
+object SExp {
+  def parse(s: String): SExp = {
+    val (rest, r) = doParse(s)
+    val s2 = rest.strip()
+    if(s2.isEmpty) r else throw new Exception(s"parse error: remaining $s2")
+  }
+
+  private def doParse(s: String): (String, SExp) = {
+    val (s1, s2) = s.strip().span(x=> x.isLetterOrDigit || x == '_' || x == '-')
+    if(s1.nonEmpty) return (s2, s1)
+    if(s2.isEmpty) throw new Exception("Invalid SExp")
+    val c = s2.head
+    val s3 = s2.tail
+    c match {
+      case '(' => {
+        val xs = Seq.newBuilder[SExp]
+        var rest = s3
+        while(!(rest.isEmpty || rest.head == ')' || rest.head == '.')){
+          val (s4, x) = doParse(rest)
+          xs += x
+          rest = s4.strip()
+        }
+        rest.head match {
+          case ')' => (rest.tail, convertList(xs.result()))
+          case '.' => {
+            val (s4, x) = doParse(rest.tail)
+            val s5 = s4.strip()
+            if(s5.head != ')') throw new Exception("Invalid SExp")
+            (s5.tail, convertListDot(xs.result(), x))
+          }
+        }
+      }
+      case '\'' => {
+        val (s4, x) = doParse(s3)
+        (s4, list("quote", x))
+      }
+    }
+  }
+}
+
 implicit val U$SExp: Unifier[SExp] = U$Union[Cons, Unit, String]
 
 implicit val I$SExp: Inspector[SExp] = I$Union(I$Cons, I$Unit, I$String)
@@ -46,6 +86,7 @@ private def convertListDot(xs: Seq[VarOr[SExp]]): VarOr[SExp] = {
   val tail = xs.tail
   if(tail.isEmpty) head else cons(head, convertListDot(tail))
 }
+private def convertListDot(xs: Seq[VarOr[SExp]], x: SExp): SExp = convertListDot(xs :+ x).asInstanceOf[SExp]
 object list {
   def apply(xs: VarOr[SExp]*): SExp = convertList(xs)
   def unapplySeq(x: VarOr[SExp]): Option[Seq[VarOr[SExp]]] = x match {

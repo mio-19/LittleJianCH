e5e4217cb489f4f30d64a1ecf10144d1a7be9e25
ㄗㄠˋ ㄑㄧˊ <tsao-chi@the-lingo.org>
2022-10-05 14:22:44 +0800

    up

--- littlejian/src/main/scala/littlejian/data/Nat.scala
+++ littlejian/src/main/scala/littlejian/data/Nat.scala
@@ -3,9 +3,14 @@ package littlejian.data
 import littlejian._
 import littlejian.ext._
 
-enum Nat derives Unifier :
-  case Zero
-  case Succ(prev: VarOr[Nat])
+sealed trait Nat derives Unifier
+
+case object Zero extends Nat derives Unifier {
+  override def toString = "0"
+}
+
+final case class Succ(prev: VarOr[Nat]) extends Nat derives Unifier {
+}
 
 object Nat {
   def from(n: Int): Nat =
@@ -16,9 +21,9 @@ object Nat {
 
 implicit class VarOrNatOps[T](self: VarOr[Nat]) {
   def +(other: VarOr[Nat]): Rel[Nat] = conde(
-    (self === Nat.Zero) >> other,
+    (self === Zero) >> other,
     for {
-      prev <- self.is[Nat](Nat.Succ(_))
+      prev <- self.is[Nat](Succ(_))
       sum <- prev + other
     } yield sum)
 
@@ -28,29 +33,29 @@ implicit class VarOrNatOps[T](self: VarOr[Nat]) {
   } yield result
 
   def *(other: VarOr[Nat]): Rel[Nat] = conde(
-    (self === Nat.Zero) >> Nat.Zero,
-    (other === Nat.Zero) >> Nat.Zero,
+    (self === Zero) >> Zero,
+    (other === Zero) >> Zero,
     for {
-      prev <- self.is[Nat](Nat.Succ(_))
+      prev <- self.is[Nat](Succ(_))
       product <- prev * other
       sum <- product + other
     } yield sum)
 
   def >(other: VarOr[Nat]): Rel[Boolean] = conde(
-    (self === Nat.Zero) >> false,
+    (self === Zero) >> false,
     for {
-      prev <- self.is[Nat](Nat.Succ(_))
-      otherPrev <- other.is[Nat](Nat.Succ(_))
+      prev <- self.is[Nat](Succ(_))
+      otherPrev <- other.is[Nat](Succ(_))
       result <- prev > otherPrev
     } yield result,
     for {
-      _ <- self.is[Nat](Nat.Succ(_))
-      _ <- other === Nat.Zero
+      _ <- self.is[Nat](Succ(_))
+      _ <- other === Zero
     } yield true)
 
   def <(other: VarOr[Nat]): Rel[Boolean] = other > self
 
-  def >=(other: VarOr[Nat]): Rel[Boolean] = Nat.Succ(self) > other
+  def >=(other: VarOr[Nat]): Rel[Boolean] = Succ(self) > other
 
-  def <=(other: VarOr[Nat]): Rel[Boolean] = Nat.Succ(other) > self
+  def <=(other: VarOr[Nat]): Rel[Boolean] = Succ(other) > self
 }

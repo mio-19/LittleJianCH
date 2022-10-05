9271ef071cd58eec6f9931c5820c531f5ced1d6c
ㄗㄠˋ ㄑㄧˊ <tsao-chi@the-lingo.org>
2022-10-05 10:52:44 +0800

    use derives

--- littlejian/src/main/scala/littlejian/data/Nat.scala
+++ littlejian/src/main/scala/littlejian/data/Nat.scala
@@ -2,17 +2,9 @@ package littlejian.data
 
 import littlejian._
 
-sealed trait Nat
-
-implicit val U$Nat: Unifier[Nat] = U$Union[Zero.type, Succ].asInstanceOf[Unifier[Nat]]
-
-case object Zero extends Nat
-
-implicit val U$Zero: Unifier[Zero.type] = equalUnifier
-
-final case class Succ(prev: VarOr[Nat]) extends Nat with Product1[VarOr[Nat]]
-
-implicit val U$Succ: Unifier[Succ] = U$Product
+enum Nat derives Unifier:
+  case Zero
+  case Succ(prev: VarOr[Nat])
 
 object Nat {
   def from(n: Int): Nat =

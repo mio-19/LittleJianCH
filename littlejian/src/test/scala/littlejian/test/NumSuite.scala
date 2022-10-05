9c3f20cea9652eabb65e0092b4a11fb3429745eb
ㄗㄠˋ ㄑㄧˊ <tsao-chi@the-lingo.org>
2022-10-05 09:08:27 +0800

    tests

--- littlejian/src/test/scala/littlejian/test/NumSuite.scala
+++ littlejian/src/test/scala/littlejian/test/NumSuite.scala
@@ -14,5 +14,6 @@ class NumSuite extends munit.FunSuite {
     assertEquals(Set.from(run[Int16] { x => Int16.from(30) + Int16.from(4) === x }), Set("34"))
     assertEquals(Set.from(run[Int32] { x => Int32.from(30) + Int32.from(4) === x }), Set("34"))
     assertEquals(Set.from(run[Int16] { x => Int16.from(30) + x === Int16.from(33) }), Set("3"))
+    assertEquals(Set.from(run[Int16] { x => x + x === x + Int16.from(1) }), Set("1"))
   }
 }

446820135ccc5c9e8a18f70c859ffc64f4512eb6
ㄗㄠˋ ㄑㄧˊ <tsao-chi@the-lingo.org>
2022-10-05 09:07:12 +0800

    tests

--- littlejian/src/test/scala/littlejian/test/NumSuite.scala
+++ littlejian/src/test/scala/littlejian/test/NumSuite.scala
@@ -10,12 +10,9 @@ import littlejian.unifier.*
 
 class NumSuite extends munit.FunSuite {
   test("numbers") {
-    assertEquals(Set.from(run[Int8] { x =>
-      for {
-        v <- Int8.from(30).get
-        _ <- x === v
-      } yield ()
-    }), Set("30"))
     assertEquals(Set.from(run[Int8] { x => Int8.from(30) + Int8.from(4) === x }), Set("34"))
+    assertEquals(Set.from(run[Int16] { x => Int16.from(30) + Int16.from(4) === x }), Set("34"))
+    assertEquals(Set.from(run[Int32] { x => Int32.from(30) + Int32.from(4) === x }), Set("34"))
+    assertEquals(Set.from(run[Int16] { x => Int16.from(30) + x === Int16.from(33) }), Set("3"))
   }
 }

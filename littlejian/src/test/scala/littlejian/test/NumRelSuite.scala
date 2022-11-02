88d08887ff2bd3e367bd00cd8ffe19cb0282eef5
ㄗㄠˋ ㄑㄧˊ <tsao-chi@the-lingo.org>
2022-11-02 20:42:56 +0800

    up

--- littlejian/src/test/scala/littlejian/test/NumRelSuite.scala
+++ littlejian/src/test/scala/littlejian/test/NumRelSuite.scala
@@ -9,6 +9,9 @@ import littlejian.ext.*
 class NumRelSuite extends munit.FunSuite {
   test("num") {
     assertEquals(Set.from(run[Int] { x => x < 9 && x > 1 }), Set("2", "3", "4", "5", "6", "7", "8"))
+    assertEquals(Set.from(run[Int] { x => x < 9999 && x > 11 }), Set(
+      """$1
+11 < $1 < 9999"""))
   }
 
 }

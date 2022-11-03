d6242d4e44e59026aa9cdf5b5451253742307acb
ㄗㄠˋ ㄑㄧˊ <tsao-chi@the-lingo.org>
2022-11-03 19:09:59 +0800

    up

--- littlejian/src/main/scala/littlejian/examples/wasm/runtime.scala
+++ littlejian/src/main/scala/littlejian/examples/wasm/runtime.scala
@@ -2,3 +2,12 @@ package littlejian.examples.wasm
 import littlejian._
 import littlejian.ext._
 import littlejian.data._
+
+enum RuntimeValue derives Unify:
+  case I32(x: VarOr[Int])
+  case I64(x: VarOr[Int])
+  case F32(x: VarOr[Float])
+  case F64(x: VarOr[Double])
+  // TODO: 128
+
+

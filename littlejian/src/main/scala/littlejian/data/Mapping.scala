da1aeb4fce8aa4f468bdf8b5ec3408bacfe7ba5b
ㄗㄠˋ ㄑㄧˊ <tsao-chi@the-lingo.org>
2022-11-03 18:53:41 +0800

    up

---
+++ littlejian/src/main/scala/littlejian/data/Mapping.scala
@@ -0,0 +1,42 @@
+package littlejian.data
+
+import littlejian._
+import littlejian.ext._
+
+sealed trait Mapping[K, V] derives Unify {
+  def get(k: K)(implicit uK: Unify[K], uV: Unify[V]): Rel[V]
+
+  def getOption(k: K)(implicit uK: Unify[K], uV: Unify[V]): Rel[Option[V]]
+
+  def notContains(k: K)(implicit uK: Unify[K]): Goal
+
+  def updated(key: K, value: V): Mapping[K, V] = MappingNonEmpty(key, value, this)
+}
+
+final case class MappingEmpty[K, V]() extends Mapping[K, V] derives Unify {
+  override def get(k: K)(implicit uK: Unify[K], uV: Unify[V]): Rel[V] = Rel.failure
+
+  override def getOption(k: K)(implicit uK: Unify[K], uV: Unify[V]): Rel[Option[V]] = None
+
+  override def notContains(k: K)(implicit uK: Unify[K]): Goal = Goal.success
+}
+
+final case class MappingNonEmpty[K, V](key: K, value: V, next: Mapping[K, V]) extends Mapping[K, V] derives Unify {
+  override def get(k: K)(implicit uK: Unify[K], uV: Unify[V]): Rel[V] = compare(key, k) {
+    value
+  } {
+    next.get(k)
+  }
+
+  override def getOption(k: K)(implicit uK: Unify[K], uV: Unify[V]): Rel[Option[V]] = compare(key, k) {
+    Some(value)
+  } {
+    next.getOption(k)
+  }
+
+  override def notContains(k: K)(implicit uK: Unify[K]): Goal = compare(key, k) {
+    Goal.failure
+  } {
+    next.notContains(k)
+  }
+}

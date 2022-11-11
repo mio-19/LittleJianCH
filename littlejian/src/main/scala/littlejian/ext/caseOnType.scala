2fa25a32aedf34d1c458cfaf333abee1aa1af7ac
ㄗㄠˋ ㄑㄧˊ <tsao-chi@the-lingo.org>
2022-11-11 21:20:27 +0800

    up

--- littlejian/src/main/scala/littlejian/ext/caseOnType.scala
+++ littlejian/src/main/scala/littlejian/ext/caseOnType.scala
@@ -16,8 +16,17 @@ implicit class CaseOnTypeOps[T, R](x: VarOr[T]) {
   def caseOnType[U <: T, A](maker: VarOr[A] => U)(isType: VarOr[A] => Rel[R])(isNotType: Rel[R])
                            (implicit t: ClassTag[U], u: Unify[U], r: Unify[R], uu: Unify[T]) = conde(
     for {
-      a <- x.is[A](maker.asInstanceOf[VarOr[A] => T])
+      a <- x.is[A](maker)
       r <- isType(a)
+    } yield r,
+    x.isNotType[U] >> isNotType
+  )
+
+  def caseOnType[U <: T, A, B](maker: (VarOr[A], VarOr[B]) => U)(isType: (VarOr[A], VarOr[B]) => Rel[R])(isNotType: Rel[R])
+                              (implicit t: ClassTag[U], u: Unify[U], r: Unify[R], uu: Unify[T]) = conde(
+    for {
+      (a, b) <- x.is[A, B](maker)
+      r <- isType(a, b)
     } yield r,
     x.isNotType[U] >> isNotType
   )

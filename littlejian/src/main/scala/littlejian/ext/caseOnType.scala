06cc97911cab42b6af9e49c180c33ec26732f163
ㄗㄠˋ ㄑㄧˊ <tsao-chi@the-lingo.org>
2022-11-13 06:47:48 +0000

    up

--- littlejian/src/main/scala/littlejian/ext/caseOnType.scala
+++ littlejian/src/main/scala/littlejian/ext/caseOnType.scala
@@ -13,7 +13,7 @@ implicit class CaseOnTypeOps[T, R](x: VarOr[T]) {
     x.isNotType[U] >> isNotType
   )
 
-  def caseOnType[U <: T, A](maker: VarOr[A] => U)(isType: VarOr[A] => Rel[R])(isNotType: Rel[R])
+  def isOr[U <: T, A](maker: VarOr[A] => U)(isType: VarOr[A] => Rel[R])(isNotType: Rel[R])
                            (implicit t: ClassTag[U], u: Unify[U], r: Unify[R], uu: Unify[T]) = conde(
     for {
       a <- x.is[A](maker)
@@ -22,11 +22,22 @@ implicit class CaseOnTypeOps[T, R](x: VarOr[T]) {
     x.isNotType[U] >> isNotType
   )
 
-  def caseOnType[U <: T, A, B](maker: (VarOr[A], VarOr[B]) => U)(isType: (VarOr[A], VarOr[B]) => Rel[R])(isNotType: Rel[R])
+  def isOr[U <: T, A, B](maker: (VarOr[A], VarOr[B]) => U)(isType: (VarOr[A], VarOr[B]) => Rel[R])(isNotType: Rel[R])
                               (implicit t: ClassTag[U], u: Unify[U], r: Unify[R], uu: Unify[T]) = conde(
     for {
       (a, b) <- x.is[A, B](maker)
       r <- isType(a, b)
+    } yield r,
+    x.isNotType[U] >> isNotType
+  )
+
+  def caseOnType[U <: T, A](maker: VarOr[A] => U)(isType: U => Rel[R])(isNotType: Rel[R])
+                           (implicit t: ClassTag[U], u: Unify[U], r: Unify[R], uu: Unify[T]) = conde(
+    for {
+      a <- fresh[A]
+      made = maker(a)
+      _ <- x === made
+      r <- isType(made)
     } yield r,
     x.isNotType[U] >> isNotType
   )

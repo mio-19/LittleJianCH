5d3b4d582facacb4dd39c2fecf4e8b254ba27505
ㄗㄠˋ ㄑㄧˊ <tsao-chi@the-lingo.org>
2022-11-22 14:19:20 +0000

    up

--- littlejian/src/main/scala/littlejian/data/sexp/sexp.scala
+++ littlejian/src/main/scala/littlejian/data/sexp/sexp.scala
@@ -23,7 +23,20 @@ implicit val I$SExpLambda: Inspect[SExpLambda] = (rec, self, x) => InspectResult
 
 implicit def sExpVector(v: Vector[_ <: VarOr[SExp]]): SExpVector = SExpVector(v)
 
-implicit def sExpLambda(fn: ((xs: Seq[_ <: VarOr[SExp]]) => VarOr[SExp])): SExpLambda = SExpLambda(fn)
+implicit def sExpLambda(fn: ((xs: Seq[VarOr[SExp]]) => VarOr[SExp])): SExpLambda = SExpLambda(fn)
+
+implicit def sExpLambda1(fn: ((a: VarOr[SExp]) => VarOr[SExp])): SExpLambda = sExpLambda({
+  case Seq(a) => fn(a)
+  case _ => throw new IllegalArgumentException("Not 1 arg")
+})
+implicit def sExpLambda2(fn: ((a: VarOr[SExp], b: VarOr[SExp]) => VarOr[SExp])): SExpLambda = sExpLambda({
+  case Seq(a, b) => fn(a, b)
+  case _ => throw new IllegalArgumentException("Not 2 arg")
+})
+implicit def sExpLambda3(fn: ((a: VarOr[SExp], b: VarOr[SExp], c: VarOr[SExp]) => VarOr[SExp])): SExpLambda = sExpLambda({
+  case Seq(a, b, c) => fn(a, b, c)
+  case _ => throw new IllegalArgumentException("Not 3 arg")
+})
 
 implicit val U$SExp: Unify[SExp] = U$Union[Cons, BoxedUnit, String, BigDecimal, SExpVector, SExpLambda].asInstanceOf
 

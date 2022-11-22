177d0b5652d044e87b7374b8d220b7098e0a397d
ㄗㄠˋ ㄑㄧˊ <tsao-chi@the-lingo.org>
2022-11-22 15:47:36 +0000

    up

--- littlejian/src/main/scala/littlejian/racket/racket.scala
+++ littlejian/src/main/scala/littlejian/racket/racket.scala
@@ -1,10 +1,16 @@
 package littlejian.racket
 
-import littlejian.data.sexp._
-import scala.collection.mutable.HashMap
+import littlejian._
+import littlejian.data.sexp.*
 
-final case class Env(stack: List[HashMap[String, SExp]]) {
-  def lookup(x: String): Option[SExp] = {
+import scala.collection.mutable
+
+type SExpr = VarOr[SExp]
+
+final case class Env(stack: List[mutable.HashMap[String, SExpr]]) {
+  def child: Env = Env(new mutable.HashMap[String, SExpr]() :: stack)
+
+  def lookup(x: String): Option[SExpr] = {
     var s = stack
     while (s.nonEmpty) {
       val head = s.head
@@ -19,27 +25,60 @@ final case class Env(stack: List[HashMap[String, SExp]]) {
     None
   }
 
-  def update(key: String, value: SExp): Unit = stack.head.update(key, value)
+  def update(key: String, value: SExpr): Unit = stack.head.update(key, value)
 }
 
 object LIST {
-  def apply(xs: SExp*) = list(xs*)
-  def unapplySeq(x: SExp): Option[Seq[SExp]] = x match {
+  def apply(xs: SExpr*) = list(xs *)
+
+  def unapplySeq(x: SExpr): Option[Seq[SExpr]] = x match {
     case () => Some(Seq())
-    case Cons(x:SExp, xs:SExp) => unapplySeq(xs).map(x+:_)
+    case Cons(x, xs) => unapplySeq(xs).map(x +: _)
     case _ => None
   }
 }
 
-def eval(env: Env, exp: SExp): SExp = exp match {
+object LISTrest {
+  def unapply(x: SExpr): Option[(List[SExpr], SExpr)] = x match {
+    case Cons(x, xs) => unapply(xs).map { case (ys, z) => (x +: ys, z) }
+    case other => Some((Nil, other))
+  }
+}
+
+object ARGS {
+  def unapply(x: SExpr): Option[(List[String], Option[String])] = x match {
+    case rest: String => Some((Nil, Some(rest)))
+    case Cons(head: String, rest) => unapply(rest).map { case (xs, y) => (head +: xs, y) }
+    case () => Some((Nil, None))
+    case _ => None
+  }
+}
+
+def parseArgs(xs: List[String], rest: Option[String], args: Seq[SExpr], env: Env): Unit = (xs, rest) match {
+  case (Nil, Some(id)) => env.update(id, list(args *))
+  case (Nil, None) if args.isEmpty => env
+  case (x :: xs, rest) if args.nonEmpty => {
+    env.update(x, args.head)
+    parseArgs(xs, rest, args.tail, env)
+  }
+  case _ => throw new IllegalArgumentException("Invalid arguments")
+}
+
+def eval(env: Env, exp: SExpr): SExpr = exp match {
   case list("define", name: String, body: SExp) => {
     env.update(name, eval(env, body))
     ()
   }
-  case LIST("begin", xs@ _* ) => evalBegin(env, xs.toList)
+  case LIST("begin", xs@_*) => evalBegin(env, xs.toList)
+  case LIST("lambda", args, body*) => args match {
+    case ARGS(xs, rest) => SExpLambda(argVec => {
+      val env0 = env.child
+      parseArgs(xs, rest, argVec, env0)
+    })
+  }
 }
 
-def evalBegin(env: Env, xs: List[SExp]): SExp = xs match {
+def evalBegin(env: Env, xs: List[SExpr]): SExpr = xs match {
   case Nil => ()
   case x :: Nil => eval(env, x)
   case x :: xs => {
